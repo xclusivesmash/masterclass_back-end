fb3787eb8fed68d415256909c6a94f5be95d1659
xclusivesmash <sbumatshiane916@gmail.com>
2025-04-07 01:27:41 +0000

    Added node installation bash file

---
+++ node_installation.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# node installation file
+
+# update the system
+sudo apt update
+# install node.js
+sudo apt install nodejs
+# install npm (node package manager)
+sudo apt install npm
