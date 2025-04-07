206d2791195d90575da6def301185902e93b7562
xclusivesmash <sbumatshiane916@gmail.com>
2025-04-07 01:29:46 +0000

    Updated changes

---
+++ node_basics/01-hello_world/index.js
@@ -0,0 +1,9 @@
+console.log("Hellow node js");
+const array = [1, 2, 3, 4, 5];
+console.log(array, "my array");
+
+setTimeout(() => {
+	console.log("This message is delayed by 2 seconds");
+}, 2000);
+
+console.log("This is the last line of sync. code");
