e8c77fd45db3aafc98ce3c73e214930e36e6deb8
xclusivesmash <sbumatshiane916@gmail.com>
2025-04-21 18:21:56 +0200

    updated the main file

--- bookstore_api/server.js
+++ bookstore_api/server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // connect to the database
 connectToDatabase();
+// middleware -> express.json()
 app.use(express.json());
 // routes here
 app.use("/api/books", router);
