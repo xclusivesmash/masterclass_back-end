c07ae86d15306d629f8cbc83a7255d4bee00a9ac
xclusivesmash <sbumatshiane916@gmail.com>
2025-04-21 18:22:22 +0200

    Updated the database file

--- bookstore_api/database/database.js
+++ bookstore_api/database/database.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 
 const PASSWORD = encodeURIComponent(process.env.PASS_CODE);
 const USERNAME = process.env.USER_NAME;
-const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.hqkwudo.mongodb.net/`
-const connectToDatabase = () => {
-    mongoose.connect(URL)
-    .then(() => {
-        console.log("Connected to database sucessfully");
-    })
-    .catch((error) => {
+const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster01.4f1qyco.mongodb.net/`;
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(URL);
+        console.log("Connected successfully to the database");
+    } catch(error) {
         console.error(error);
-    });
+        process.exit(1);
+    };
 }
 
 module.exports = connectToDatabase;
