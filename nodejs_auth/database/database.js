ea89d2a78129f6437eb636ded79b54090c3dce8d
xclusivesmash <sbumatshiane916@gmail.com>
2025-04-22 22:59:46 +0200

    Added the database connection file

---
+++ nodejs_auth/database/database.js
@@ -0,0 +1,16 @@
+// establish connection to database
+const mongoose = require("mongoose");
+const username = process.env.USER_NAME;
+const password = encodeURIComponent(process.env.PASS_CODE);
+const connectToDatabase = async () => {
+    try {
+        // connection establishment
+        await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.j2ps4ru.mongodb.net/`);
+        console.log("Connected successsfully to database");
+    } catch(error) { // connection failed.
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectToDatabase;
