8d44dd6af8cb6c7d62ad1a523426d27272fd05b1
xclusivesmash <sbumatshiane916@gmail.com>
2025-04-22 23:45:23 +0200

    Updated main entry file

--- nodejs_auth/server.js
+++ nodejs_auth/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDatabase = require("./database/database");
+const authRouter = require("./routers/auth_routers");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ connectToDatabase();
 
 // use the express middleware: express.json()
 app.use(express.json());
+app.use("/api/auth", authRouter);
 
 // start the server
 app.listen(PORT, "localhost", () => {
